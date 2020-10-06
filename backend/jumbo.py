a4df52e69a3ec59922bd3d158f262564941a8f42
diegosilva789 <56441292+diegosilva789@users.noreply.github.com>
2020-10-06 18:36:41 -0300

    Parcialmente resolvido

--- backend/jumbo.py
+++ backend/jumbo.py
@@ -7,55 +7,82 @@ class Jumbo():
 
 
     def __init__(self):
-        self.conectado = False
         pass
 
 
     # funcao para conectar-se ao banco
     def conectar(self, host, porta, usuario, database, senha):
         try:
-            print("Iniciando a conexão")
+            print("\tIniciando a conexão")
+            print("\t%s %s %s %s %s" % (host, porta, usuario, database, senha))
             self.conn = connect(user="%s" % (usuario),
                                 password="%s" % (senha),
                                 host="%s" % (host),
                                 port="%s" % (porta),
                                 database="%s" % (database))
-            self.conectado = True
+            print("\tConectado")
             return True
         except:
+            print("\tErro de Conexão")
             return False
 
 
     # funcao para inserir shapefile no banco
     def inserir(self, parametrizar, host, usuario, database, senha, arquivo_shp_end, arquivo_shp, tabela):
-        print("Inserido os dados na tabela selecionada")
-        programa = r"C:\Program Files\QGIS 3.10\bin\ogr2ogr.exe"
-        # command = r'start cmd /c ""%s" -f "PostgreSQL" -nlt MULTIPOINT -append -fieldmap %s PG:"host=%s user=%s dbname=%s password=%s" "%s""' % (programa, parametrizar, host, usuario, database, senha, arquivo_shp)
-        command = r'start cmd /c ""%s" -f "PostgreSQL" -nlt MULTIPOINT -sql "%s from %s" PG:"host=%s user=%s dbname=%s password=%s" "%s" -nln %s"' % (
-            programa, parametrizar, arquivo_shp[:-4], host, usuario, database, senha, arquivo_shp_end, tabela)
-        print(command)
-        return os.system(command,)
+        print("\tInserido os dados na tabela selecionada")
+        print("\t%s" % (arquivo_shp_end))
+        print("\t%s" % (arquivo_shp))
+        print("\t%s" % (tabela))
+        sf = shapefile.Reader(arquivo_shp_end)
+        tipo = sf.shapeTypeName 
+        if tipo == "MULTIPOINT" or tipo == "POINT":
+            print("\tTipo: %s" % (sf.shapeTypeName))
+            programa = r"C:\Program Files\QGIS 3.10\bin\ogr2ogr.exe"
+            command = r'start cmd /c ""%s" -f "PostgreSQL" -nlt MULTIPOINT -sql "%s from %s" PG:"host=%s user=%s dbname=%s password=%s" "%s" -nln %s"' % (
+                programa, parametrizar, arquivo_shp[:-4], host, usuario, database, senha, arquivo_shp_end, tabela)
+            print("\t%s" % (command))
+            print ("\tSucesso")
+            return os.system(command)
+        elif tipo == "POLYLINE":
+            print("\tTipo: %s" % (sf.shapeTypeName))
+            programa = r"C:\Program Files\QGIS 3.10\bin\ogr2ogr.exe"
+            command = r'start cmd /c ""%s" -f "PostgreSQL" -nlt MULTILINESTRING -sql "%s from %s" PG:"host=%s user=%s dbname=%s password=%s" "%s" -nln %s"' % (
+                programa, parametrizar, arquivo_shp[:-4], host, usuario, database, senha, arquivo_shp_end, tabela)
+            print("\t%s" % (command))
+            print ("\tSucesso")
+            return os.system(command)
+        elif tipo == "POLYGON":
+            print("\tTipo: %s" % (sf.shapeTypeName))
+            programa = r"C:\Program Files\QGIS 3.10\bin\ogr2ogr.exe"
+            command = r'start cmd /c ""%s" -f "PostgreSQL" -nlt MULTIPOLYGON -sql "%s from %s" PG:"host=%s user=%s dbname=%s password=%s" "%s" -nln %s"' % (
+                programa, parametrizar, arquivo_shp[:-4], host, usuario, database, senha, arquivo_shp_end, tabela)
+            print("\t%s" % (command))
+            print ("\tSucesso")
+            return os.system(command)
+        return "\tTipo Incompatível: %s" % (sf.shapeTypeName)
 
 
     # funcao para capturar tabelas existentes no banco
     def get_tables(self):
-        print("Lendo nomes das tabelas do banco")
-        self.tables = []
+        print("\tLendo nomes das tabelas do banco")
+        tabelas = []
         conn = self.conn
         tab_cursor = conn.cursor()
-        tab_cursor.execute("""SELECT table_name FROM information_schema.tables
-        WHERE table_schema = 'public'""")
+        tab_cursor.execute(r"SELECT table_name FROM information_schema.tables WHERE table_schema = 'public'")
         tab_names = (tab_cursor.fetchall())
         for tup in tab_names:
-            self.tables += [tup[0]]
+            tabelas += [tup[0]]
         # tab_cursor.close()
-        return self.tables[3:]
+        print ("\t%s" % (tabelas[3:]))
+        print ("\tSucesso")
+        return tabelas[3:]
 
 
     # funcao para capturar nome das colunas do banco
     def get_col_names_db(self, tabela):
-        print("Lendo nomes das colunas da tabela selecionada")
-        columns = []
+        print("\tLendo nomes das colunas da tabela selecionada")
+        print("\t%s" % (tabela))
+        colunas_db = []
         conn = self.conn
         col_cursor = conn.cursor()
         col_names_str = r"SELECT column_name FROM INFORMATION_SCHEMA.COLUMNS WHERE table_name = '%s';" % (
@@ -63,40 +90,38 @@ class Jumbo():
         col_cursor.execute(col_names_str)
         col_names = (col_cursor.fetchall())
         for tup in col_names:
-            columns += [tup[0]]
+            colunas_db += [tup[0]]
         # col_cursor.close()
-        return columns[1:-1]
+        print("\t%s" % (colunas_db[1:-1]))
+        print ("\tSucesso")
+        return colunas_db[1:-1]
 
 
     # funcao para capturar nome das colunas do shapefile
     def get_col_names_shp(self, arquivo_shp_end):
-        print("Lendo nomes das colunas do arquivo shapefile")
+        print("\tLendo nomes das colunas do arquivo shapefile")
+        print("\t%s" % (arquivo_shp_end))
         sf = shapefile.Reader(arquivo_shp_end)
-        file = list(field[0] for field in sf.fields)
-        return file[1:]
+        colunas_shp = list(field[0] for field in sf.fields)
+        print("\t%s" % (colunas_shp[1:]))
+        print ("\tSucesso")
+        return colunas_shp[1:]
 
 
     # funcao para capturar selecao feita pelo usuario
     def parametrizar(self, colunas_selecionadas, colunas_tabela):
-        print("Tratando a parametrização")
+        print("\tTratando a parametrização")
+        print("\t%s" % (colunas_selecionadas))
+        print("\t%s" % (colunas_tabela))
         select_sql_string = 'SELECT'
-        as_sql_string = ' as'
         for x in range((len(colunas_selecionadas))):
             if colunas_selecionadas[x] != '':
-                select_sql_string += ' %s' % (colunas_tabela[x])
-                as_sql_string += ' %s' % (colunas_selecionadas[x])
-                if x < (len(colunas_selecionadas) - 1):
-                    select_sql_string += ','
-                    as_sql_string += ','
+                select_sql_string += ' %s as %s,' % (colunas_selecionadas[x], colunas_tabela[x])
         if select_sql_string[-1] == ',':
-            select_sql_string = select_sql_string.replace(',','')
-        if as_sql_string[-1] == ',':
-            as_sql_string = as_sql_string.replace(',','')
-        select_sql_string += as_sql_string
-        print (select_sql_string)
+            select_sql_string = select_sql_string[:-1]
+        print ("\tSucesso")
         return select_sql_string
 
 
     def close(self):
         self.conn = None
-        self.conectado = False
