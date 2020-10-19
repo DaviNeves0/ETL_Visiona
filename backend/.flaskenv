5d00e98a4a8d93e1c479607affd539d81b743108
diegosilva789 <56441292+diegosilva789@users.noreply.github.com>
2020-10-19 09:37:55 -0300

    deletando

--- backend/.flaskenv
+++
@@ -1,2 +0,0 @@
-FLASK_APP=programa.py
-FLASK_ENV=development
--- frontend/src/services/api.js
+++
@@ -1,7 +0,0 @@
-import axio from 'axios';
-
-const api = axio.create({
-    baseURL: 'http://localhost:5000'
-})
-
-export default api; 
